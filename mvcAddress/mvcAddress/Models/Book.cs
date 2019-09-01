e9ec303b5ae604dc80e8ce8bf94ac6fdf8217d8b
Paulo <44127888+paulopazb@users.noreply.github.com>
2019-09-01 08:32:32 -0400

    Segundo Commit

---
+++ mvcAddress/mvcAddress/Models/Book.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mvcAddress.Models
+{
+    public enum TypeContact
+    {
+       Phone,
+       Email,
+       Snapchat,
+       Facebook,
+       Instagram,
+       Dblink,
+
+    }
+    public class Book
+    {
+        [Key]
+        public int BookID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public TypeContact Type { get; set; }
+        [Required]
+        public string Contact { get; set; }
+
+
+    }
+}
