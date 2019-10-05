22d81df87002cc8edba60329db081503c2dc9b21
Paulo <44127888+paulopazb@users.noreply.github.com>
2019-10-05 09:48:52 -0400

    Error al instalar nuevos nuggets

---
+++ mvcAddress/appAddress/appAddress/Models/Book.cs
@@ -0,0 +1,27 @@
+﻿namespace appAddress.Models
+{
+  
+    public enum TypeContact
+    {
+        Phone,
+        Email,
+        Snapchat,
+        Facebook,
+        Instagram,
+        Dblink,
+
+    }
+    public class Book
+    {
+        [Key]
+        public int BookID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public TypeContact Type { get; set; }
+        [Required]
+        public string Contact { get; set; }
+
+
+    }
+}
